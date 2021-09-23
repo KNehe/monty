eed93c3dcecc6ebf40b04e649ee7bb300abe34b0
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-09-23 06:01:16 +0000

    fix pop error message

--- pop.c
+++ pop.c
@@ -13,7 +13,7 @@ void pop(stack_t **stack, unsigned int line_no)
 
 	if (stack == NULL || *stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_no);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_no);
 		exit(EXIT_FAILURE);
 	}
 	if ((*stack)->next != NULL)
